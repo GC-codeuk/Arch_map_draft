94d9330888eb8097fa391fa9b8266f7dc0be942e
Gavin <gavincassidy@yahoo.co.uk>
2016-05-22 21:36:21 +0100

    Format plotdetails for mobile and add swipe

--- app/assets/javascripts/plot_details.js
+++ app/assets/javascripts/plot_details.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $('.icon-menu').click(function() {
-    	$('#menu').animate({left: 0}, 'slow')
+      $('#menu').animate({left: 0}, 'slow')
   });
   $('.icon-close').click(function() {
-    	$('#menu').animate({left: -430}, 'slow')
+      $('#menu').animate({left: -430}, 'slow')
+  });
+
+  $("#plotdetails").on("swipeleft",function(){
+      $('#map').animate({ width: $('#map').width() + 215 }, 450);
+      $('#plotdetails').animate({left: - 430}, 500).toggleClass( "hidden" );
   });
 
   /*$('.icon-close').click(function() {
